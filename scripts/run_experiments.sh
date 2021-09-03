38476bea37e68484f6356a3e7c132bc78027569c
TinaBBB <tinashen.tianshu@gmail.com>
2021-09-02 22:37:30 -0400

    Update require and start to edit bash scripts

---
+++ scripts/run_experiments.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Running the baseline models
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner baseline.sh 1 0
+
+# Training the recommender part
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner both.sh 1 0
+
+# Training the dialog part
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner t2t_rec_rgcn.sh 1 0
